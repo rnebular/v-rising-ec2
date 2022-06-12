6286baaf09130d013890ff2f5b78dbf77f79fd7e
rnebular <jtbyrum@gmail.com>
2022-06-11 20:11:54 -0700

    update with script from server

--- source/start_server.ps1
+++ source/start_server.ps1
@@ -8,10 +8,14 @@ $steamcmd_check=Test-Path -Path "C:\Program Files\steamcmd"
 if ($steamcmd_check -eq $False) {
     Write-Output "SteamCMD not found, installing."
     Install-SteamCMD
+} else {
+    Write-Output "SteamCMD found, moving on."
+    Start-Sleep 1
 }
 
 Write-Output "Installing/Updating V-Rising dedicated server."
 Update-SteamApp -AppID 1829350 -Path 'C:\v-rising' -Force
 
 Write-Output "Starting V Rising Dedicated Server"
-Start-Service -Name "V-Rising Server"
+start-sleep 1
+Start-Process -FilePath "C:\v-rising\VRisingServer.exe" -Wait -NoNewWindow
